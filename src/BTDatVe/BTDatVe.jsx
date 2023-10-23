9dcbe775d89c1ea0e299d9d151d663f3e77c5a39
binhnguyen <leechiupin@gmail.com>
2023-10-23 22:04:56 +0900

    Xây dựng cấu trúc jsx, sửa datajson và thêm bg image

---
+++ src/BTDatVe/BTDatVe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import data from "./data.json"
+
+export const BTDatVe = () => {
+    
+    console.log('data: ', data);
+  return (
+    <div className='container mt-3'>
+        BTDatVe
+    </div>
+  )
+}
---
+++ src/BTDatVe/ChairList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ChairList = () => {
+  return (
+    <div>ChairList</div>
+  )
+}
