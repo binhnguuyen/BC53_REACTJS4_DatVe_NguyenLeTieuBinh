5621ba773322549f7208ebcf433e068b39751b76
binhnguyen <leechiupin@gmail.com>
2023-10-25 22:23:11 +0900

    Thêm BS 5.3.2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { BTDatVe } from './BTDatVe/BTDatVe'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="description" tabIndex="1">
